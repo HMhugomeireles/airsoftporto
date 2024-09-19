d7ef19ff57be0e4a2b3ed2b5ec458b5fcb699838
HMhugomeireles <meireles.hmm@mail.com>
2024-09-19 20:44:09 +0100

    auth validation

---
+++ src/app/api/event/ticket/validation/route.ts
@@ -0,0 +1,36 @@
+import { prisma } from "@/lib/prisma";
+import { redirect } from "next/navigation";
+import { NextResponse } from "next/server";
+
+export async function GET(req: Request) {
+  try {
+    const url = new URL(req.url);
+    const ticketId = url.searchParams.get('t');
+    const eventId = url.searchParams.get('e');
+
+    if (!ticketId || ! eventId) {
+      return NextResponse.json({ message: "Not found" }, { status: 400 });
+    }
+
+    const event = await prisma.event.findFirst({
+      where: {
+        id: eventId
+      }
+    })
+    const ticket = await prisma.ticket.findFirst({
+      where: {
+        id: ticketId
+      }
+    })
+
+    if (event === null || ticket === null || !event?.id && !ticket?.id) {
+      return NextResponse.json({ message: "Not found" }, { status: 400 });
+    }
+
+    return redirect(`/dashboard/event/ticket/${event.id}/${ticket.id}`)
+  } catch (error) {
+    console.error('Error validate ticket:', error);
+
+    return NextResponse.json({ message: "Error validate ticket" }, { status: 500 });
+  }
+}
---
+++ src/app/api/oauth/google/route.ts
@@ -0,0 +1,92 @@
+import { ROLES } from '@/lib/constants';
+import { lucia } from '@/lib/lucia';
+import { prisma } from '@/lib/prisma';
+import { googleOAuthClient } from '@/lib/providers/googleOauth';
+import { cookies } from "next/headers";
+import { redirect } from 'next/navigation';
+import { NextRequest } from 'next/server';
+import { v4 as uuidv4 } from 'uuid';
+
+interface GoogleUser {
+  id: string;
+  email: string;
+  verified_email: string;
+  name: string;
+  given_name: string;
+  family_name:string
+  picture: string;
+  locale: string;
+}
+
+export async function GET(req: NextRequest, res: Response) {
+  try {
+    
+    const url = new URL(req.url);
+    const searchParams = url.searchParams;
+  
+    const code = searchParams.get('code');
+    const state = searchParams.get('state');
+  
+    if (!code || !state) {
+      return Response.json({ error: "Invalid request!1" }, { status: 400 })
+    }
+  
+    const savedState = cookies().get('state')?.value
+    const codeVerifier = cookies().get('codeVerifier')?.value
+  
+    if (!codeVerifier || !savedState) {
+      console.error('no code verifier or state')
+      return new Response('Invalid Request!2', { status: 400 })
+    }
+  
+    if (state !== savedState) {
+      console.error('state mismatch')
+      return new Response('Invalid Request!3', { status: 400 })
+    }
+  
+    const { accessToken, idToken } = await googleOAuthClient.validateAuthorizationCode(code, codeVerifier)
+    const googleResponse = await fetch('https://www.googleapis.com/oauth2/v1/userinfo', {
+      headers: {
+        Authorization: `Bearer ${accessToken}`
+      },
+      method: 'GET'
+    })
+  
+    const googleData = (await googleResponse.json()) as any
+  
+    const existingUser = await prisma.user.findUnique({
+      where: {
+        email: googleData.email
+      } // need update the user date from the provider
+    })
+  
+    const userId = existingUser 
+      ? existingUser.id
+      : (await createUser(googleData))
+  
+    const session = await lucia.createSession(userId, {})
+    const sessionCookie = await lucia.createSessionCookie(session.id)
+    cookies().set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes)
+  
+    return redirect(`/onboarding/${userId}`)
+  } catch (error) {
+    console.log(error)
+    return redirect('/')
+  }
+}
+
+async function createUser(userData: GoogleUser) {
+  const user = await prisma.user.create({
+    data: {
+      id:  uuidv4(),
+      email: userData.email,
+      picture: userData.picture,
+      provider: 'google',
+      firstName: userData.given_name,
+      lastName: userData.family_name,
+      role: [ROLES.USER],
+      permissions: []
+    }
+  })
+  return user.id
+}
