411c8b34bb6e6c2efae789ecf10fce36dbee8256
HMhugomeireles <meireles.hmm@mail.com>
2024-10-19 00:09:59 +0100

    testing api routing

--- src/app/api/test/team/route.ts
+++ src/app/api/test/team/route.ts
@@ -9,51 +9,68 @@ type CreateTeamBody = {
 
 export async function POST(request: Request) {
   try {
-    const body = request.formData as unknown as CreateTeamBody
+    const body: CreateTeamBody = await request.json()
 
     if (!body) {
       return NextResponse.json({ message: "Missing team name" }, { status: 400 });
     }
 
-
-    const isExistTeam = await prisma.team.findFirst({
+    const user = await prisma.user.findFirst({
       where: {
-        name: body.name
+        id: body.userId
       }
     })
 
-    if (isExistTeam) {
-      return NextResponse.json({ message: "This are previous register" }, { status: 400 });
-    }
-
-    const player = await prisma.user.findFirst({ where: { id: body.userId }, include: { TeamMember: true, TicketPlayer: true } })
-
-    if (!player) {
+    if (!user) {
       return NextResponse.json({ message: "user not found" }, { status: 400 });
     }
 
-    const teamCreated = await prisma.team.create({
+    const teamMember = await prisma.teamMember.create({
       data: {
         id: randomUuid(),
-        name: body.name,
-        members: {
-          create: {
-            id: randomUuid(),
-            player: {
-              connect: {
-
-              }
+        Team: {
+          connectOrCreate: {
+            where: {
+              name: body.name
+            },
+            create: {
+              id: randomUuid(),
+              name: body.name
             }
+          }
+        },
+        player: {
+          connect: {
+            id: user.id
           }
         }
       }
     })
 
-    return NextResponse.json(teamCreated);
-
+    return NextResponse.json(teamMember);
   } catch (error) {
     console.error('Error create Team:', error);
 
     return NextResponse.json({ message: "Failed to create Team" }, { status: 500 });
+  }
+}
+
+
+export async function GET(request: Request) {
+  try {
+    const team = await prisma.team.findMany({
+      include: {
+        members: {
+          include: {
+            player: true
+          }
+        }
+      }
+    })
+
+    return NextResponse.json({team});
+  } catch (error) {
+    console.log(error)
+    return NextResponse.json({});
   }
 }
